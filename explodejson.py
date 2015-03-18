df64214040a5b598438d216ca74cad63035e4735
Gus Maskowitz <gus@rackspace.com>
2015-03-18 13:51:48 +0000

    tuned comments

--- explodejson.py
+++ explodejson.py
@@ -1,10 +1,9 @@
-# /tmp/waldotest-5be56476922d4d258d665e27a7a533cc.json
-
 import json
 import sys
 import os.path
 import os
 
+# Take a filename as input. Assuming the filename ends with .json so cutting it
 base = os.path.basename(sys.argv[1])[0:-5]
 os.mkdir(base)
 discovery = json.load(open(sys.argv[1]))
@@ -15,4 +14,3 @@ for key in discovery:
     json.dump(value, fp, sort_keys=True, indent=2)
     fp.close()
     
-
